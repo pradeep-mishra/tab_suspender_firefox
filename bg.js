d7e4df2e35dfca33138fab77c1db7c256e80dcb3
Pradeep Mishra <>
2020-02-06 00:33:38 +0530

    version 1.1

--- bg.js
+++ bg.js
@@ -1,55 +1,111 @@
-let tabs = { };
-let timeoutCount = 5000;
+let tabs = {};
+let timeoutCount = 300000;
+let suspendApp = "false";
 
-browser.storage.local.get("timeoutCount").then(function(i){
-	console.log('timeoutCount is', i.timeoutCount);
-	timeoutCount = Number(i.timeoutCount);
-});
-
-
-browser.tabs.onCreated.addListener(function(tab){
-	console.log('added tab', tab.id);
-	if(!tabs[tab.id]){
-		tabs[tab.id] = { };	
-	}
-	tabs[tab.id].tab = tab;
-});
-
-browser.tabs.onRemoved.addListener(function(tabId){
-	if(tabs[tabId] && tabs[tabId].timeout){
-		try{
-			clearTimeout(tabs[tabId].timeout);
-		}catch(e){
+async function loadScript() {
+	browser.storage.onChanged.addListener(function (i) {
+		if (i.timeoutCount) {
+			//console.log('timecount changed');
+			timeoutCount = Number(i.timeoutCount.newValue);
+		} else if (i.suspendApp) {
+			//console.log('i.suspendApp', i.suspendApp)
+			suspendApp = i.suspendApp.newValue == "true" ? "true" : "false";
+			//console.log('suspendApp', suspendApp);
 		}
-	}
-	tabs[tabId] = { };
-	console.log('removed tabId', tabId);
-});
+	});
 
-browser.tabs.onActivated.addListener(function(tab){
-	//tabs[i] = null;
-	console.log('activated tab', tab.tabId);
-
-	if(tabs[tab.tabId] && tabs[tab.tabId].timeout){
-		try{
-			clearTimeout(tabs[tab.tabId].timeout);
-		}catch(e){
+	browser.storage.local.get("timeoutCount").then(function (i) {
+		if (i && i.timeoutCount) {
+			//console.log('timecount changed');
+			timeoutCount = Number(i.timeoutCount);
 		}
-		tabs[tab.tabId].timeout = null;
-	}
+	});
 
-	if(tab.previousTabId){
-
-		console.log('adding timeout for suspending tab', tab.previousTabId);	
-
-		if(!tabs[tab.previousTabId]){
-			tabs[tab.previousTabId] = { };
+	browser.storage.local.get("suspendApp").then(function (i) {
+		if (i && i.suspendApp) {
+			//console.log('timecount changed');
+			suspendApp = i.suspendApp == "true" ? "true" : "false";
 		}
-		tabs[tab.previousTabId].timeout = setTimeout(function(tabId){
-			console.log('suspened tab', tabId);
-			browser.tabs.discard(tabId);
-		}, timeoutCount,tab.previousTabId);
+	});
 
-	}	
 
-})
+
+	browser.tabs.onCreated.addListener(function (tab) {
+		//console.log('added tab', tab.id);
+		if (!tabs[tab.id]) {
+			tabs[tab.id] = {};
+		}
+		tabs[tab.id].tab = tab;
+	});
+
+	browser.tabs.onRemoved.addListener(function (tabId) {
+		if (tabs[tabId] && tabs[tabId].timeout) {
+			try {
+				clearTimeout(tabs[tabId].timeout);
+			} catch (e) {}
+		}
+		tabs[tabId] = {};
+		//console.log('removed tabId', tabId);
+	});
+
+	browser.tabs.onActivated.addListener(async function (tab) {
+		//tabs[i] = null;
+		//console.log('activated tab', tab.tabId);
+
+		if (tabs[tab.tabId] && tabs[tab.tabId].timeout) {
+			try {
+				clearTimeout(tabs[tab.tabId].timeout);
+			} catch (e) {}
+			tabs[tab.tabId].timeout = null;
+		}
+
+		//console.log('suspendApp is', suspendApp)
+
+		if (suspendApp == "true") {
+			return true;
+		}
+
+		if (tab.previousTabId) {
+
+			let isLive = await getTabValue(tab.previousTabId, "live");
+			//console.log('isLive', isLive);
+
+			if (isLive == "1") {
+				//console.log('previous tab', tab.previousTabId, 'is live can bee suspended');
+				return true;
+			}
+
+			//console.log('tab', tab.previousTabId, 'will be suspnded after', (timeoutCount / 1000), 'seconds');
+
+			if (!tabs[tab.previousTabId]) {
+				tabs[tab.previousTabId] = {};
+			}
+			tabs[tab.previousTabId].timeout = setTimeout(async function (tabId) {
+				if (suspendApp == "true") {
+					return true;
+				}
+				let currentTabId = await getCurrentTabId();
+				let isSuspendedOff = await getTabValue(currentTabId, "live");
+				//console.log('isSuspendedOff', isSuspendedOff);
+				//console.log('suspened tab', tabId);
+				browser.tabs.discard(tabId);
+			}, timeoutCount, tab.previousTabId);
+
+		}
+
+	});
+}
+
+async function getCurrentTabId() {
+	let tabArray = await browser.tabs.query({
+		currentWindow: true,
+		active: true
+	});
+	return tabArray[0].id;
+}
+
+async function getTabValue(tabId, key) {
+	return await browser.sessions.getTabValue(tabId, key);
+}
+
+loadScript();
--- popup.js
+++ popup.js
@@ -1,14 +1,71 @@
+async function loadScript() {
+	let elmTimeoutSelect = document.getElementById("timeout_select");
+	let elmSuspendAll = document.getElementById("suspend_all");
+	let elmCurrentTab = document.getElementById("current_tab");
 
-
-
-document.getElementById("timeout_select").addEventListener('change',function(e){
-	var elm = e.target;
-	console.log('changed to', elm.value);
-	browser.storage.local.set({
-	  timeoutCount:  elm.value
+	let tabArray = await browser.tabs.query({
+		currentWindow: true,
+		active: true
 	});
-});
+	let currentTabId = tabArray[0].id;
+	//console.log('currentTabId', currentTabId);
 
-browser.storage.local.get("timeoutCount").then(function(i){
-	document.getElementById("timeout_select").value = i.timeoutCount;
-});
+	elmTimeoutSelect.addEventListener('change', function (e) {
+		var elm = e.target;
+		//console.log('changed to', elm.value);
+		browser.storage.local.set({
+			timeoutCount: elm.value
+		});
+	});
+
+	elmCurrentTab.addEventListener('change', async function (e) {
+		var elm = e.target;
+		if (elm.checked == true) {
+			browser.sessions.setTabValue(currentTabId, "live", "1");
+			//console.log('setting tab seesion key live to 1');
+		} else {
+			browser.sessions.setTabValue(currentTabId, "live", "0");
+			//console.log('setting tab seesion key live to 0');
+		}
+	});
+
+	elmSuspendAll.addEventListener('change', function (e) {
+		var elm = e.target;
+		//console.log('changed to', elm.value);
+		let val = "false";
+		if (elm.checked == true) {
+			val = "true";
+		}
+		//console.log('elm.checked', elm.checked)
+		//console.log('sending suspend app val', val)
+		browser.storage.local.set({
+			suspendApp: val
+		});
+	});
+
+
+	browser.storage.local.get("timeoutCount").then(function (i) {
+		//console.log('ii', i)
+		if (i && i.timeoutCount) {
+			elmTimeoutSelect.value = i.timeoutCount;
+		}
+	});
+
+	browser.storage.local.get("suspendApp").then(function (i) {
+		if (i && i.suspendApp) {
+			elmSuspendAll.checked = i.suspendApp == "true" ? true : false;;
+		}
+	});
+
+	let liveVal = await getTabValue(currentTabId, "live");
+	//console.log('current tab is live', liveVal);
+	if (liveVal == "1") {
+		elmCurrentTab.checked = true;
+	}
+}
+
+async function getTabValue(tabId, key) {
+	return await browser.sessions.getTabValue(tabId, key);
+}
+
+loadScript();
